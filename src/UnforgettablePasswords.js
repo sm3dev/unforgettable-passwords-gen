e35a3dbf8a642b40c6cb2b85dcc2966c8d864c47
Michael Wright <info@saymoremultimedia.com>
2022-02-08 14:39:02 -0600

    mw-create-application-component-6 imported navBar and footer components into the application component UnforgettablePasswords.js

---
+++ src/UnforgettablePasswords.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Footer } from "./components/Footer";
+import { NavBar } from "./components/NavBar";
+
+export const UnforgettablePasswords = () => {
+  return (
+    <div>
+      <NavBar />      <h1>this is the Unforgettable Passwords Generator</h1>
+
+      <Footer />
+    </div>
+  );
+};
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NavBar = () => {
+  return <div>this nav bar will be here
+    <div>GitHub</div>
+    <div>Linkedin</div>
+    <div>Twitter</div>
+  </div>
+};
